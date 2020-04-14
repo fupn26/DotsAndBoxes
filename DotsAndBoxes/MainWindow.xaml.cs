02e2c629bbb7cc2010aefa1371c3d0ef9bc79125
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-04-14 23:00:15 +0200

    Draw points on the Canvas

--- DotsAndBoxes/MainWindow.xaml.cs
+++ DotsAndBoxes/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace DotsAndBoxes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int GameWidth = 100;
+        private const int GameHeight = 100;
+
+        private readonly int NumberOfPoints;
+
+        private List<Point> points;
+        private List<Ellipse> ellipses;
+
         public MainWindow()
         {
             InitializeComponent();
+            NumberOfPoints = (int)canvas.Width / GameWidth * (int)canvas.Height / GameHeight;
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            for (int i = 0; i < NumberOfPoints/2; ++i)
+            {
+                for (int j = 0; j < NumberOfPoints/2; ++j)
+                {
+                    DrawPoint(i*GameHeight, j*GameWidth);
+                }
+            }
+        }
+
+        private void DrawPoint(int x, int y)
+        {
+            // Create pen.
+            Pen blackPen = new Pen(Brushes.Black, 3);
+
+            // Create rectangle for ellipse.
+//            Rectangle rect = new Rectangle(0, 0, 200, 100);
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Height = 10;
+            ellipse.Width = 10;
+            ellipse.Fill = Brushes.Red;
+
+            Canvas.SetLeft(ellipse, y-10/2);
+            Canvas.SetTop(ellipse, x-10/2);
+            canvas.Children.Add(ellipse);
+            ellipses.Add(ellipse);
+
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
